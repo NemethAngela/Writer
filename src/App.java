ffc18c39c225dcccf61a2c1780e2ef0adc22c978
Nemeth Angela <nemethangela75@gmail.com>
2023-11-22 18:58:13 +0100

    init

--- src/App.java
+++ src/App.java
@@ -1,3 +1,13 @@
+/*
+* File: app.js
+* Author: Németh Angéla
+* Copyright: 2023, Németh Angéla
+* Group: Szoft 2-1 E
+* Date: 2023-11-22
+* Github: https://github.com/NemethAngela/
+* Licenc: GNU GPL
+*/
+
 /*Feladat 004
 Adott a következő interfész:
 
--- src/IWritable.java
+++ src/IWritable.java
@@ -1,3 +1,13 @@
+/*
+* File: app.js
+* Author: Németh Angéla
+* Copyright: 2023, Németh Angéla
+* Group: Szoft 2-1 E
+* Date: 2023-11-22
+* Github: https://github.com/NemethAngela/
+* Licenc: GNU GPL
+*/
+
 import java.io.IOException;
 
 public interface IWritable {
